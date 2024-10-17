4ce08ba9510041f4090b0ebb91dc569ef8728dbc
Hamza Elghonemy <137107646+Hamza-Elghonemy@users.noreply.github.com>
2024-10-18 00:09:52 +0300

    First Commit

---
+++ MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MagicVanilla_CouponAPI.Models;
+using MagicVanilla_CouponAPI.Models.DTO;
+
+namespace MagicVanilla_CouponAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
+            CreateMap<Coupon, CouponDTO>().ReverseMap();
+        }
+    }
+}
